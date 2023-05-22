1d29778fa365527d276b26a4d40d6e01f06c2e75
timxx21 <112845568+timxx21@users.noreply.github.com>
2023-05-22 16:15:36 +0300

    Update csrf.php

--- csrf.php
+++ csrf.php
@@ -1,1 +1,8 @@
+// На стороне сервера необходимо сравнить значение токенов
+session_start();
 
+if (isset($_POST['csrf_token']) && $_POST['csrf_token'] === $_SESSION['csrf_token']) {
+   // токены совпали
+} else {
+   // токены не совпали
+}
