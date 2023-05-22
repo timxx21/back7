2333446aef4c5889ee94526c125cfed0eaa21956
timxx21 <112845568+timxx21@users.noreply.github.com>
2023-05-22 16:37:22 +0300

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,6 +1,29 @@
 // Логин и пароль экранируются.
-$login = mysql_real_escape_string($_POST[’user’]);
+$login = mysql_real_escape_string($_POST[’login’]);
 $password = mysql_real_escape_string($_POST[’pass’]);
 $query = "SELECT * FROM users WHERE login = ’" .
-$user . "’ AND pass = ’" . $pass . "’";
+$login . "’ AND pass = ’" . $pass . "’";
 $result = mysql_query($query);
+
+
+
+if(!empty($login) and !empty($pass)){
+        try{
+            $check=$db1->prepare("SELECT * FROM login WHERE login=?");
+            $check->bindParam(1,$login);
+            $check->execute();
+            $username=$check->fetchALL();
+        //    echo '<pre>';
+       //     print_r($username);
+      //      var_dump($username);
+     //       echo '</pre>';
+            if(password_verify($pass,$username[0][2])){
+                $uid=$username[0]['id'];
+                $error=FALSE;
+            }
+        }
+        catch(PDOException $e){
+            print('Error : ' . $e->getMessage());
+            exit();
+        }
+    }
