944d720011353a527cfc812ad69cadfca27914b7
timxx21 <112845568+timxx21@users.noreply.github.com>
2023-05-22 16:04:00 +0300

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,1 +1,6 @@
-
+// Логин и пароль экранируются.
+$login = mysql_real_escape_string($_POST[’login’]);
+$password = mysql_real_escape_string($_POST[’pass’]);
+$query = "SELECT * FROM users WHERE login = ’" .
+$login . "’ AND pass = ’" . $pass . "’";
+$result = mysql_query($query);
